c286c20848a74a7308ed542a8891923149217e9c
erichs890 <erichs890@gmail.com>
2024-06-09 18:16:42 -0300

    mash nao sei

--- app/src/main/java/com/example/trabalho32/removerObra.kt
+++ app/src/main/java/com/example/trabalho32/removerObra.kt
@@ -1,59 +1,67 @@
 package com.example.trabalho32
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [removerObra.newInstance] factory method to
- * create an instance of this fragment.
- */
 class removerObra : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var obraList: ListView
+    private lateinit var obras: MutableList<String>
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        db = FirebaseFirestore.getInstance()
+        obras = mutableListOf()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_remover_obra, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_remover_obra, container, false)
+        obraList = view.findViewById(R.id.obra_list)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment removerObra.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            removerObra().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        // Recuperar obras do Firestore
+        db.collection("obras")
+            .get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    for (document in task.result!!) {
+                        obras.add(document.id)
+                    }
+                    val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, obras)
+                    obraList.adapter = adapter
+                } else {
+                    Log.d("Erro", "Erro ao recuperar obras: ${task.exception}")
                 }
             }
+
+        // Adicionar listener para o botão de exclusão
+        obraList.setOnItemClickListener { _, _, position, _ ->
+            val obraId = obras[position]
+            db.collection("obras")
+                .document(obraId)
+                .delete()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        obras.removeAt(position)
+                        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, obras)
+                        obraList.adapter = adapter
+                        Toast.makeText(requireContext(), "Obra removida com sucesso!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Log.d("Erro", "Erro ao excluir obra: ${task.exception}")
+                    }
+                }
+        }
+
+        return view
     }
 }
