8e01018752203d2e5b3aa96c2157d4953966786d
erichs890 <erichs890@gmail.com>
2024-06-09 19:07:42 -0300

    mash nao sei

--- app/src/main/java/com/example/trabalho32/removerObra.kt
+++ app/src/main/java/com/example/trabalho32/removerObra.kt
@@ -12,56 +12,100 @@ import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
 
 class removerObra : Fragment() {
+
     private lateinit var obraList: ListView
+
     private lateinit var obras: MutableList<String>
+
     private lateinit var db: FirebaseFirestore
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         db = FirebaseFirestore.getInstance()
+
         obras = mutableListOf()
+
     }
+
 
     override fun onCreateView(
+
         inflater: LayoutInflater, container: ViewGroup?,
+
         savedInstanceState: Bundle?
+
     ): View? {
+
         val view = inflater.inflate(R.layout.fragment_remover_obra, container, false)
+
         obraList = view.findViewById(R.id.obra_list)
 
-        // Recuperar obras da coleção "Obras2"
-        db.collection("Obras2")
+
+        // Recuperar obras do Firestore
+
+        db.collection("obras")
+
             .get()
+
             .addOnCompleteListener { task ->
+
                 if (task.isSuccessful) {
+
                     for (document in task.result!!) {
+
                         obras.add(document.id)
+
                     }
+
                     val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, obras)
+
                     obraList.adapter = adapter
+
                 } else {
+
                     Log.d("Erro", "Erro ao recuperar obras: ${task.exception}")
+
                 }
+
             }
 
+
         // Adicionar listener para o botão de exclusão
+
         obraList.setOnItemClickListener { _, _, position, _ ->
+
             val obraId = obras[position]
-            db.collection("Obras2")
+
+            db.collection("obras")
+
                 .document(obraId)
+
                 .delete()
+
                 .addOnCompleteListener { task ->
+
                     if (task.isSuccessful) {
+
                         obras.removeAt(position)
-                        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, obras)
-                        obraList.adapter = adapter
-                        Toast.makeText(requireContext(), "Obra removida com sucesso!", Toast.LENGTH_SHORT).show()
+
+                        obraList.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, obras)
+
                     } else {
+
                         Log.d("Erro", "Erro ao excluir obra: ${task.exception}")
+
                     }
+
                 }
+
         }
 
+
         return view
+
     }
+
 }
