66f0f2c20dd2350d51a1148a9b717845edbcf6c0
gabrcelm2 <gabrielcosta123454321@gmail.com>
2024-06-09 21:58:51 -0300

    oie

--- app/src/main/java/com/example/trabalho32/GerenVisitas.kt
+++ app/src/main/java/com/example/trabalho32/GerenVisitas.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -22,9 +23,10 @@ import com.google.firebase.firestore.util.Util
 
 class GerenVisitas : Fragment() {
 
-    private lateinit var db: DatabaseReference
     private lateinit var visitasRecyclerView: RecyclerView
     private lateinit var visitList: ArrayList<Visitas>
+    private lateinit var searchView: SearchView
+    private lateinit var filteredVisitList: ArrayList<Visitas>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +39,20 @@ class GerenVisitas : Fragment() {
         visitasRecyclerView.setHasFixedSize(true)
 
         visitList = arrayListOf<Visitas>()
+
+        searchView = view.findViewById(R.id.pesquisa)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filteredVisitList = filterVisitas(newText)
+                val vAdapter = VisitasAdapter(filteredVisitList)
+                visitasRecyclerView.adapter = vAdapter
+                return true
+            }
+        })
 
         getVisitasData()
 
@@ -54,13 +70,28 @@ class GerenVisitas : Fragment() {
                     val visitas = document.toObject(Visitas::class.java)
                     visitList.add(visitas)
                 }
-                val vAdapter = VisitasAdapter(visitList)
+                this.visitList = visitList
+                filteredVisitList = visitList
+                val vAdapter = VisitasAdapter(filteredVisitList)
                 visitasRecyclerView.adapter = vAdapter
             } else {
                 Log.w("Visitas", "Error getting data", task.exception)
             }
         }
     }
+
+    private fun filterVisitas(query: String?): ArrayList<Visitas> {
+        val filteredList = ArrayList<Visitas>()
+        for (visita in visitList) {
+            if (visita.tipo!!.contains(query!!, ignoreCase = true) ||
+                visita.data!!.contains(query, ignoreCase = true) ||
+                visita.hora!!.contains(query, ignoreCase = true)) {
+                filteredList.add(visita)
+            }
+        }
+        return filteredList
+    }
+
 }
 
 
